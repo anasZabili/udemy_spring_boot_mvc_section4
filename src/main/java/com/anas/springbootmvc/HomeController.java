61224a91c84f0bcaf1c950951c5c6a2a854e6950
anas zabili <anaszab78@gmail.com>
2021-03-14 17:48:48 +0100

    lesson 26

--- src/main/java/com/anas/springbootmvc/HomeController.java
+++ src/main/java/com/anas/springbootmvc/HomeController.java
@@ -1,7 +1,12 @@
 package com.anas.springbootmvc;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
@@ -10,5 +15,30 @@ public class HomeController {
   public String home() {
     System.out.println("Home page requested");
     return "index.jsp";
+  }
+
+  @RequestMapping("add")
+  // spring boot inject lobjet httpserver
+  // RequestParam inject le variable d'identifant nb1
+  // recurperé dans l'url dans nb1
+  public ModelAndView add(@RequestParam("nb1") int nb1, 
+    @RequestParam("nb2")int nb2) {
+      // , HttpSession session
+
+    // le model view permet de factoriser le code
+    // en prenant en parameter le nom de la vue souhaité
+    // et les données
+    ModelAndView mv = new ModelAndView();
+    mv.setViewName("result.jsp");
+
+    int res = nb1 + nb2;
+    mv.addObject("res", res);
+
+    // set attibut met dans les variable de session
+    // "res" de valeur res
+    // session.setAttribute("res", res);
+
+    // return "result.jsp";
+    return mv;
   }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -8,5 +8,13 @@ pageEncoding="ISO-8859-1"%>
   </head>
   <body>
     Welcome to My app
+    <form action="add">
+      <label>Enter the first number</label>
+      <input type="text" name="nb1" />
+      <br />
+      <label>Enter the second number</label>
+      <input type="text" name="nb2" />
+      <input type="submit" />
+    </form>
   </body>
 </html>
